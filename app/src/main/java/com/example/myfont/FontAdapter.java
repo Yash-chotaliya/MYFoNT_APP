14a39dbea3183dae896a5497642a74113392adb4
yash chotaliya <yashchotaliya187@gmail.com>
2022-12-10 00:13:04 +0530

    " copy button in recycle view in action"

--- app/src/main/java/com/example/myfont/FontAdapter.java
+++ app/src/main/java/com/example/myfont/FontAdapter.java
@@ -2,16 +2,13 @@ package com.example.myfont;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class FontAdapter extends RecyclerView.Adapter<FontAdapter.ViewHolder> {
