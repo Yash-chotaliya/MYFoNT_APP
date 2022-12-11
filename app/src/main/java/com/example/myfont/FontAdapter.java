f26ca1c6c562e40658fe20c1ccf28cfcc72f0050
yash chotaliya <yashchotaliya187@gmail.com>
2022-12-11 12:51:05 +0530

    " set text font in recycle view"

--- app/src/main/java/com/example/myfont/FontAdapter.java
+++ app/src/main/java/com/example/myfont/FontAdapter.java
@@ -2,6 +2,7 @@ package com.example.myfont;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,19 +26,43 @@ public class FontAdapter extends RecyclerView.Adapter<FontAdapter.ViewHolder> {
     @Override
     public FontAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fontlay,parent,false);
-        ViewHolder vh = new ViewHolder(view);
-        return vh;
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FontAdapter.ViewHolder holder, int position) {
+        Typeface[] arr = {Typeface.createFromAsset(context.getAssets(), "Fonts/3dlet.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/3Dumb.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/ahundredmiles.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/akchyar_unicode_bold.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/ampikunanti.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Ananda_1_Md.TTF"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/arachnid.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Archipelago.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Astalamet.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Autobahn.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/BalaCynwyd.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/BallTack.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Barbatrick.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Baubau.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Bauchaomaicha.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Billionaire.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Blockbuster.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Castle.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Caddy.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Cenobyte.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Chunkfive.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Courgette.ttf"),
+                Typeface.createFromAsset(context.getAssets(), "Fonts/Datacut.ttf")};
+        holder.font.setTypeface(arr[position]);
         holder.font.setText(s);
+
         holder.copybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String s = holder.font.getText().toString();
+                String s1 = holder.font.getText().toString();
                 ClipboardManager clipboardManager = (ClipboardManager) holder.itemView.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData data = ClipData.newPlainText("data",s);
+                ClipData data = ClipData.newPlainText("data",s1);
                 clipboardManager.setPrimaryClip(data);
 
                 Toast.makeText(context, "copied text", Toast.LENGTH_SHORT).show();
@@ -47,7 +72,7 @@ public class FontAdapter extends RecyclerView.Adapter<FontAdapter.ViewHolder> {
 
     @Override
     public int getItemCount() {
-        return 20;
+        return 23;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
